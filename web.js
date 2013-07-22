d25802527f5add74a9da81186abd85e0cf6d1fc3
Michel Angeles <michel.angeles@gmail.com>
2013-07-22 10:15:37 +0000

    UPdated web.js read file

--- web.js
+++ web.js
@@ -1,9 +1,24 @@
 var express = require('express');
+var fs = require('fs'); 
+var buffer = new Buffer(8);
+
 var app = express();
 app.use(express.logger());
 
+
+
+
+fs.readSync('index.html','utf8',function(err,data){
+if (err){
+    return console.log(err);
+ 
+}
+buffer.write(data,"utf-8");
+}
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'))
 });
 
 var port = process.env.PORT || 5000;
