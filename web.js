0f2e80780d519013ba8a764de33569fce3a079c3
Michel Angeles <michel.angeles@gmail.com>
2013-07-22 14:10:17 +0000

    Change 14

--- web.js
+++ web.js
@@ -1,28 +1,12 @@
 var express = require('express');
 var fs = require('fs');
-var buffer= new Buffer(25);
 var app = express();
 app.use(express.logger());
-
-
-
-
-fs.readFileSync('index.html','utf8',function(err,data){
-if (err){
-    return console.log(err);
-
-}
-buffer.write(data,"utf-8");
-//streaming = data;
-});
-
-
+var buffer = fs.readFileSync('index.html','utf8');
+var stringBuf = buffer.toString();
 app.get('/', function(request, response) {
-  response.send(buffer.toString());
+  response.send(stringBuf);
 });
-
-
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
